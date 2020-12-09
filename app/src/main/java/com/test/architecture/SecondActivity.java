84f268c3be45c40a48cc9601f511e2129d7949aa
danhhh-anya <anyachan123321@gmail.com>
2020-12-09 17:33:19 +0200

    init commit

--- app/src/main/java/com/test/architecture/SecondActivity.java
+++ app/src/main/java/com/test/architecture/SecondActivity.java
@@ -11,7 +11,7 @@ public class SecondActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_third);
+        setContentView(R.layout.activity_second);
         btnContinue = findViewById(R.id.btnContinue);
         btnContinue.setOnClickListener(view ->{
             Intent intent= new Intent(this,ThirdActivity.class);
--- app/src/main/java/com/test/architecture/ThirdActivity.java
+++ app/src/main/java/com/test/architecture/ThirdActivity.java
@@ -2,13 +2,20 @@ package com.test.architecture;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ThirdActivity extends AppCompatActivity {
-
+    Button btnStillContinue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+        btnStillContinue = findViewById(R.id.btnStillContinue);
+        btnStillContinue.setOnClickListener(view ->{
+            Intent intent= new Intent(this,NewActivity.class);
+            startActivity(intent);
+        });
     }
 }
