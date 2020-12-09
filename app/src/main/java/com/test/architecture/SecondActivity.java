35c3f5d625cd1e9eb5398fe2f041783ab60a0385
danhhh-anya <anyachan123321@gmail.com>
2020-12-09 17:14:17 +0200

    init commit

--- app/src/main/java/com/test/architecture/SecondActivity.java
+++ app/src/main/java/com/test/architecture/SecondActivity.java
@@ -2,13 +2,21 @@ package com.test.architecture;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
-
+    Button btnContinue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_third);
+        btnContinue = findViewById(R.id.btnContinue);
+        btnContinue.setOnClickListener(view ->{
+            Intent intent= new Intent(this,ThirdActivity.class);
+            startActivity(intent);
+        });
     }
+
 }
